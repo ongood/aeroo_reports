0a36252e81e7d7886bf9aca0300c3e028ae89f75
Juan Jose Scarafia <jjs@adhoc.com.ar>
2021-10-04 13:05:23 +0000

    [MIG] base_report_to_printer_aeroo/: Migration to 13.0
    X-original-commit: af040cfb92eedc501cb6d1b095100643cb5f6df0
    Part-of: ingadhoc/aeroo_reports#39

--- base_report_to_printer_aeroo/__manifest__.py
+++ base_report_to_printer_aeroo/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Report to print Aeroo',
-    'version': '11.0.1.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Reporting Subsystem',
     'sequence': 14,
     'author': 'ADHOC SA',
