6c9745dc483a50adb80d897f345ae32f224fa68f
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-05-10 10:12:49 +0000

    [REM] unneeded encodings
    Part-of: ingadhoc/aeroo_reports#41

--- report_aeroo/docs_client_lib.py
+++ report_aeroo/docs_client_lib.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 ################################################################################
 #
 # Copyright (c) 2009-2014 Alistek ( http://www.alistek.com ) All Rights Reserved.
--- report_aeroo/report_parser.py
+++ report_aeroo/report_parser.py
@@ -1,4 +1,3 @@
-# -*- encoding: utf-8 -*-
 ################################################################################
 #
 #  This file is part of Aeroo Reports software - for license refer LICENSE file
